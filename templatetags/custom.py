014707bc839654978baba5c7aea94c5fae0ab346
Neel Bhavsar <neelbhavsar@Neels-MacBook-Pro.local>
2018-09-24 13:47:23 -0400

    Change user architecture

---
+++ templatetags/custom.py
@@ -0,0 +1,7 @@
+from django import template
+register = template.Library()
+
+@register.filter
+def month(value):
+	array_of_month=['January', 'February', 'March','April','May','June','July','August','September','October','November','December']
+	return array_of_month[value]
